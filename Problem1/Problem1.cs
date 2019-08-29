fdf7da5e6253b6de4d42f2a092f102f3c18d9732
azallae <azallae2@gmail.com>
2019-08-29 12:08:42 -0700

    Using HashSet in C# to find two int in an array with sum k

---
+++ Problem1/Problem1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*  Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+    Bonus: Can you do this in one pass? */
+
+namespace Problem1
+{
+	class Problem1
+	{
+        static void FindPairs(int[] arr, int k)
+        {
+            HashSet<int> s = new HashSet<int>();
+
+            foreach (int i in arr)
+            {
+                int sum = k - i;
+
+                if (s.Contains(sum))
+                {
+                    System.Console.WriteLine("Sets: {0} and {1}", sum, i);
+                }
+
+                s.Add(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {10, 15, 3, 7};
+            int k = 17;
+
+            FindPairs(arr, k);
+
+            Console.ReadKey();
+        }
+	}
+}
---
+++ Problem1/README.md
@@ -0,0 +1,14 @@
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+
+Starting off the problem, we know that we need to either have two pointers on the array to add and compare with k
+	This approach requires that the first pointer would tart at the beginning of the array while the other pointer traverses the array to see if any other number adds up to k
+	This would take O(n^2) time, let's try another approach
+
+Another approach would be to use a HashSet and the Contains method which is in O(1) time
+	Input each integer into the HashSet and check the HashSet each time to see if the current number added with HashSet.Contains equals k
+	Add the current number to the HashSet
+	output the sets of numbers that add up to k
